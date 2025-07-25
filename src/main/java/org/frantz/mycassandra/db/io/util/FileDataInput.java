aebc608868584c2327dc15f83613d81b828e6cb4
frantz <2496517957@qq.com>
2025-07-26 00:02:02 +0800

    20250721 Cassandra enhenced io tools to process bytebuffer type buffer input

---
+++ src/main/java/org/frantz/mycassandra/db/io/util/FileDataInput.java
@@ -0,0 +1,16 @@
+package org.frantz.mycassandra.db.io.util;
+
+import java.io.Closeable;
+import java.io.IOException;
+
+public interface FileDataInput extends RewindableDataInput, Closeable {
+    String getPath();
+
+    boolean isEOF() throws IOException;
+
+    long bytesRemaining() throws IOException;
+
+    void seek(long pos) throws IOException;
+
+    long getFilePointer();
+}
---
+++ src/main/java/org/frantz/mycassandra/db/io/util/RewindableDataInput.java
@@ -0,0 +1,13 @@
+package org.frantz.mycassandra.db.io.util;
+
+import java.io.IOException;
+
+public interface RewindableDataInput extends DataInputPlus{
+
+    DataPosition mark();
+
+    void reset(DataPosition mark) throws IOException;
+
+    long bytesPastMark(DataPosition mark);
+
+}

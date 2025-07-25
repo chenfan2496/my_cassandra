3ad8f7e75fa4fcf58de872123cd6c444257ebc2b
frantz <2496517957@qq.com>
2025-07-25 09:01:00 +0800

    20250721 Cassandra enhenced io tools to process bytebuffer type buffer input

--- src/main/java/org/frantz/mycassandra/utils/vints/FastByteOperations.java
+++ src/main/java/org/frantz/mycassandra/utils/vints/FastByteOperations.java
@@ -42,7 +42,6 @@ public class FastByteOperations {
         BestHolder.BEST.copy(src, srcPosition, trg, trgPosition, length);
     }
 
-
     public static void copy(ByteBuffer src, int srcPosition, ByteBuffer trg, int trgPosition, int length)
     {
         BestHolder.BEST.copy(src, srcPosition, trg, trgPosition, length);
